1d5dd2f1cf2eec56b3ffb779d2ea5a37fead80d4
timesnewronann <ronanborja26@gmail.com>
2021-01-28 01:34:19 +0000

    challenge 2

---
+++ assignment2-6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+ int main() {
+   int variable1 = 28;
+   int variable2 = 32;
+   int variable3 = 37;
+   int variable4 = 24;
+   int variable5 = 33;
+
+   int sum = (variable1 + variable2 + variable3 + variable4 + variable5); 
+   double average = sum / 5;
+
+   cout << "Sum: " << sum << endl;
+   cout << "Average: " << average << endl;
+ }
