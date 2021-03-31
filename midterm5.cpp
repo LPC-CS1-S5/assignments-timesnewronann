a192c63819ea8d863ae9f025e5c402c80418b911
timesnewronann <ronanborja26@gmail.com>
2021-03-31 16:31:48 +0000

    working on program5

---
+++ midterm5.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <fstream>
+using namespace std;
+
+void getInput(int %, int %);
+int isSame(int %, int%);
+int intDivision(int, int);
+void fileWrite(ofstream, int);
+
+int main()
+{
+
+}
+
+void getInput(int %n1, 
