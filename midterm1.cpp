87ff6d2e000c2faaad34530825a3cd67d83577cd
timesnewronann <ronanborja26@gmail.com>
2021-03-31 15:13:19 +0000

    did question 1

---
+++ midterm1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  const int N=5, M=5;
+  int i, j;
+  for(i=0;i < N; i++)
+  {
+    for(j=i; j < M; j++)
+      cout << " (" << i << "," << j << ") ";
+    cout << endl;
+  }
+}
