3b7dbc222dcc01d32b1b8a657f73cd07c93b94d9
timesnewronann <>
2021-05-04 23:38:46 +0000

    did question 1

--- QUIZ 7/question1.cpp
+++ QUIZ 7/question1.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main()
 {
   const int SIZE = 10; // size of array
-  int n[SIZE] = {-25, 0, 10, 15, 19, 20, 25, 30, 45} // array with the list of numbers 
+  int n[SIZE] = {-25, 0, 10, 15, 19, 20, 25, 30, 45}; // array with the list of numbers 
 
-  for (int i = 0; i < SIZE; i++)
+  for(int i = 0; i < SIZE; i++)
   {
     if (i != 9)
     {
@@ -19,6 +19,13 @@ int main()
       }
     }
     else if ( i == 9)
+    {
+      if (abs(n[i] - n[i-1]) < 10)
+      {
+        cout << " The absolute value of " << n[i] << " - " << n[i-1] << " = " << n[i] - n[i-1] << " is less than 10" << endl;
+        cout << "The first element is " << n[i-1] << endl;
+      }
+    }
   }
 
 }
