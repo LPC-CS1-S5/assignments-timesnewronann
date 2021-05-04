47400bb93ec29143b7f5d23df6ab4843116ee367
timesnewronann <>
2021-05-04 23:44:59 +0000

    should've used SIZE instead of rows and cols for the 2D array

--- question2.cpp
+++ question2.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 using namespace std;
 
-void printtriangle(int numbers[][3], int, int);
-const int rows = 3;
-const int cols = 3;
+void printtriangle(int numbers[][3], int);
+
 
 int main()
 {
-  int rows;
-  int cols;
-  int numbers[rows][cols] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };
+  const int SIZE = 3;
+  int numbers[SIZE][SIZE] = { {0, 1, 2}, {3, 4, 5}, {6, 7, 8} };
 
-  printtriangle(numbers, rows, cols);
+  printtriangle(numbers, SIZE);
 }
 
-void printtriangle(int numbers[][3], int rows, int cols)
+void printtriangle(int numbers[][3], int SIZE)
 {
-  for (int i = 0; i < rows; i++)
+  for (int i = 0; i < SIZE; i++)
   {
-    for (int j = 0; j < cols; j++)
+    for (int j = 0; j < SIZE; j++)
     {
       if (i >= j)
       {
