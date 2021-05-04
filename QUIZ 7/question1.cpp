e966dcf94b388118ee001e5e0c095a296b8180ed
timesnewronann <>
2021-05-04 23:33:49 +0000

    woo

--- QUIZ 7/question1.cpp
+++ QUIZ 7/question1.cpp
@@ -6,5 +6,19 @@ using namespace std;
 
 int main()
 {
-  const int SIZE = 10;
+  const int SIZE = 10; // size of array
+  int n[SIZE] = {-25, 0, 10, 15, 19, 20, 25, 30, 45} // array with the list of numbers 
+
+  for (int i = 0; i < SIZE; i++)
+  {
+    if (i != 9)
+    {
+      if (abs(n[i] - n[i+1]) < 10)
+      {
+        cout << "The absolute value of " << n[i] << " - " << n[i+1] << " = " << n[i] - n[i+1] << " is less than 10" << endl;
+      }
+    }
+    else if ( i == 9)
+  }
+
 }
