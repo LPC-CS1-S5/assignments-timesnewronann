f1eb2893235e09c4109675b2138c154bbe61f6df
timesnewronann <>
2021-05-04 23:48:50 +0000

    swag

---
+++ QUIZ 7/question3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+int findMax(int number[], int from, int to);
+const int N = 10;
+
+int main()
+{
+  int number[N] =
+  int from = 
+  int to =
+  int max;
+  int maxTwo;
+}
+
+int findMax(int number[], int from, int to)
+{
+  int max = 0;
+  int maxTwo = 0;
+  for (in)
+}
