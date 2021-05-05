3f916babba0ed01d371057cce2972dea374cc744
timesnewronann <>
2021-05-05 00:00:00 +0000

    adding some comments

--- QUIZ 7/question3.cpp
+++ QUIZ 7/question3.cpp
@@ -8,9 +8,9 @@ const int N = 10;
 
 int main()
 {
-  int number[N] =
-  int from = 
-  int to =
+  int number[N] ={10, 15, 0, -25, 19, 20, 25, 30, 45, 20};
+  int from = 0; // starting index of the range 
+  int to = N - 1; // the last index of the range 
   int max;
   int maxTwo;
 }
@@ -19,5 +19,12 @@ int findMax(int number[], int from, int to)
 {
   int max = 0;
   int maxTwo = 0;
-  for (int i = 0; i <= to)
+  for (int i = to/2; i <= to; i++)
+  {
+    if(maxTwo < num[i])
+    {
+      maxTwo = num[i];
+    }
+  }
+  return max,maxTwo;
 }
