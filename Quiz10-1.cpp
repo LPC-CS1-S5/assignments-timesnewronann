fd3ec383e9ee06f57607382ecbeac54fd3bdbe1a
timesnewronann <>
2021-05-12 05:39:18 +0000

    starting bool function

--- Quiz10-1.cpp
+++ Quiz10-1.cpp
@@ -1,1 +1,27 @@
+//In this program, we are going to count the occurrence of “th” in the paragraph.  We use the paragraph as follows, which is saved as a file with the name “python.txt”.
+
+//“The Python 2 language was officially discontinued in 2020 (first planned for 2015), and Python 2.7.18 is the last Python 2.7 release and therefore the last Python 2 release. No more security patches or other improvements will be released for it. With Python 2's end-of-life, only Python 3.5.x and later are supported. thither“
+
+
+//In order to count the number of occurrences of the keyword, we can use any kind of member function from the String class. All occurrences in a word should be counted as separate occurrences. For example, "thither" has two "th" substrings.
+
 #include <iostream>
+#include <fstream>
+#include <cctype>
+#include <cstring>
+
+using namespace std;
+bool count(char[], char[]) // count the amount of th in the paragraph
+
+int main()
+{
+
+}
+
+
+bool count(char read[], char python[])
+{
+  char *pos;
+
+  pos =
+}
