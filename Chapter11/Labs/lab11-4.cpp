37c730c716d01824c755ac61217bd498dd3e060d
timesnewronann <>
2021-05-17 06:28:45 +0000

    I need to figure out the last 3 functions still

--- Chapter11/Labs/lab11-4.cpp
+++ Chapter11/Labs/lab11-4.cpp
@@ -6,5 +6,90 @@ using namespace std;
 const int MAXSIZE = 1000;
 
 struct Namerecords{
+  string stname
+  string sex;
+  int year;
+  string name; 
+  int count;
+};
+
+int makingNameRecords(Namerecords []);
+void printNameRecords(Namerecords[], int);
+
+string getstatename(string);
+string getgender(string);
+int getyear(string);
+string getname(string);
+int getcount(string);
+
+int main()
+{
+  int numofRecords = 0;
+  Namerecords nr[MAXSIZE]; // call makingNameRecords and printNameRecords
+
+  numofRecords = makingNameRecords(nr);
+  printNameRecords(nr, numofRecords);
+
+}
+
+int makingNameRecords(Namerecords nr[])
+{
+  int cnt = 0;
+  string readline;
+  ifstream ifso;
+
+  ifso.open("cany.txt");
+  if (!ifso) {
+    cout << "File Open Error \n";
+    exit(0);
+
+  }
+  while ((ifso >> readline) && (cnt < 1000)) { // reads the lines from the text
+    nr[cnt].stname = getstatename(readline);
+    nr[cnt].sex = getgender(readline);
+    nr[cnt].year = getyear(readline);
+    nr[cnt].name = getname(readline);
+    nr[cnt].count = getcount(readline);
+    cnt++;
+  }
+  return cnt;
+}
+
+void printNameRecords(Namerecords nr[], int numofRecords)
+{
+  for(int i = 0; i < numofRecords)
+  {
+    cout << setw(5) << nr[i].stname << "\t";
+    cout << setw(5) << nr[i].sex << "\t";
+    cout << setw(5) << nr[i].year << "\t";
+    cout << setw(5) << nr[i].name << "\t";
+    cout << setw(5) << nr[i].count << endl;
+    
+  }
+}
+
+string getstatename(string str)
+{
+  return str.substr(0,2); // split the state name from the lines
+}
+
+string getgender(string str)
+{
+  int pos = str.find(',');
+  return str.substr(pos+1, 1);
+}
+
+int getyear(string str)
+{
+  int pos 
+}
+
+string getname(string str)
+{
+
+}
+
+int getcount(string str)
+{
   
 }
