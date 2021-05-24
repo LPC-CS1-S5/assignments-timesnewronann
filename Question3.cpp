0804fbdb1ac3cd7a4a852ec0beb48b75e89945df
timesnewronann <>
2021-05-24 03:00:05 +0000

    importing question3 as well

---
+++ Question3.cpp
@@ -0,0 +1,5 @@
+/*We are going to check the inclusion relationship between the two arrays. In this program, we need to check that the "numbers1" is a subset of the "numbers2"(no need to check a reverse way). We assume that two arrays "numbers1" and "numbers2" have the sorted order(increasing).
+
+If it is implemented without a nested-loop, you can get the extra points.
+tips: Two arrays have already sorted in ascending order.
+*/
