69572e96736408c7def3242e0f5138e55aff3aed
timesnewronann <>
2021-05-24 05:12:25 +0000

    exact doesn't work because it just = 0

--- Question4.cpp
+++ Question4.cpp
@@ -33,7 +33,7 @@ bool compare(int exact, string userinput, string words)
   while ( pos != -1)
   {
     pos = words.find(userinput, i);
-    cout << "Found at: " << pos+1 << " in " << words.substr(pos, 10) << endl;
+    cout << "Found at: " << pos+1 << " in " << words.substr(pos, ) << endl;
     i = pos+1;
     i++;
   }
