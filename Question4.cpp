60b33a72ef20cccc41ed3220bc13577962210eee
timesnewronann <>
2021-05-24 04:32:52 +0000

    trying to figure out how I can get this program to work

--- Question4.cpp
+++ Question4.cpp
@@ -11,7 +11,7 @@ In your main function, we will call a function to find the occurrences and then 
 #include <cstring>
 using namespace std;
 
-bool 
+bool compare(int, string, string); // bool because we will compare the int vs words 
 
 int main()
 {
@@ -20,5 +20,10 @@ int main()
   string  words= "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quibus natura iure responderit non esse verum aliunde finem beate vivendi, a se principia rei gerendae peti; Quae enim adhuc protulisti, popularia sunt, ego autem a te elegantiora desidero. Duo Reges: constructio interrete. Tum Lucius: Mihi vero ista valde probata sunt, quod item fratri puto. Bestiarum vero nullum iudicium puto. Nihil enim iam habes, quod ad corpus referas; Deinde prima illa, quae in congressu solemus: Quid tu, inquit, huc? Et homini, qui ceteris animantibus plurimum praestat, praecipue a natura nihil datum esse dicemus?";
 
     cout << "Enter user string "; 
-    cin >> userinput
+    cin >> userinput;
+}
+
+bool compare(int exact, string userinput, string words)
+{
+  
 }
