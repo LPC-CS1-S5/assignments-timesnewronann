1183ebefb8807cdd027dc58429da1a61bb2dd15d
timesnewronann <>
2021-05-24 02:58:15 +0000

    importing question 4

---
+++ Question4.cpp
@@ -0,0 +1,7 @@
+/*In this program, if the given word is even a sub-string of the original word, we will count it.
+
+For example, the given word is "some" and the original word is "something", we will count it as a word occurrence.
+If the given word occurs several times in an original word, we will count all occurrences.
+
+For example, the given is "ch" and the original word is "church", we will count it as 2 occurrences.
+In your main function, we will call a function to find the occurrences and then receive the total occurrences from that function, and then the returned result will be printed in the main function*/
