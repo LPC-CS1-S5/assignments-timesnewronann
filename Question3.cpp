4bef160c686e98c1a8e1d71541b72e3db776ecab
timesnewronann <>
2021-05-24 03:27:52 +0000

    I will try to do it without a nested loop

--- Question3.cpp
+++ Question3.cpp
@@ -17,5 +17,26 @@ int main()
   int numbers1[SIZE1] = {2, 3, 5, 9, 10}; // array with the list 
   int numbers2[SIZE2] = {1, 2, 3, 5, 9, 10, 11, 13, 15, 17}; 
 
+  if(poop(numbers1,numbers2,SIZE1,SIZE2)) 
+    cout << "Numbers1 is a subset of the numbers2" << endl; 
+  else 
+    cout << "Numbers1 is not a subset of the numbers2" << endl;
 
+
+
+}
+
+bool POOP(int numbers1[], int numbers2[], int SIZE1, int SIZE2) // function header 
+{
+  for (int i = 0; i < SIZE1; i++) // I'm not sure how to do this without a nested loop
+  {
+    for (int j = 0; j < SIZE2; j++)
+    {
+      if (numbers1[i] == numbers2[j])
+      break;
+    }
+    if ( j == SIZE2)
+      return 0;
+  }
+  return 1; 
 }
